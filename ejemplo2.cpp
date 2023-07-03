9ae61a743d298d209689419de03a4d56a4751794
kiomy18 <asilkiomybaby@gmail.com>
2023-07-03 11:41:23 -0500

    go

--- ejemplo2.cpp
+++ ejemplo2.cpp
@@ -20,9 +20,21 @@ int main()
     TipoEstructura objetoEstructura;
     TipoUnion objetoUnion;
 
-    objetoUnion.datoH = 0;
-    objetoUnion.datoL = 127;
+    objetoUnion.datoH = 0b00000000;
+    objetoUnion.datoL = 0b00000000;
+
+    objetoUnion.dato = 0;
+
     bitset<16> binario16(objetoUnion.dato);
+    string cadena = binario16.to_string();
+    int i = 0;
+    while (i > 15)
+    {
+        binario16[i] = '1';
+        i + 2;
+    }
+
+    // binario16[7]='1';
 
     cout << "contenido de dato " << objetoUnion.dato << endl;
     cout << "contenido en binario " << binario16 << endl;
